aae79aa3ef1fc1e1ae7c43a86dec1aec2cb478bb
muflih <m_muflih@hotmail.com>
2019-11-18 12:43:16 +0700

    meta tweet

--- src/components/seo.js
+++ src/components/seo.js
@@ -84,6 +84,7 @@ function SEO({ description, lang, meta, keywords, title, image }) {
         },
         {
           name: `twitter:image`,
+          property:`twitter:image`,
           content: image!==undefined?"https://mmuflih.com"+image:"https://mmuflih.com/assets/icon.png",
         },
       ]
