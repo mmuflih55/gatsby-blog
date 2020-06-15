8589379a16e525507b128168d4d7c65d039b8a35
apem <m_muflih@hotmail.com>
2020-06-15 22:26:31 +0700

    fix height

--- src/Style/Style.js
+++ src/Style/Style.js
@@ -1,162 +1,163 @@
-import { fade, makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from "@material-ui/core/styles"
 const useStyles = makeStyles(theme => ({
-    grow: {
-        flexGrow: 1,
+  grow: {
+    flexGrow: 1,
+  },
+  noBox: {
+    boxShadow: "none",
+  },
+  center: {
+    // display: 'flex',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // flexDirection: 'column'
+    alignItems: "center",
+  },
+  listItem: {
+    [theme.breakpoints.up("md")]: {
+      margin: 10,
     },
-    noBox: {
-        boxShadow: 'none'
+    display: "flex",
+    margin: 5,
+    padding: 10,
+    transition: "0.3s",
+    "&:hover": {
+      margin: 2,
+      boxShadow: "0 5px 10px grey",
     },
-    center: {
-        // display: 'flex',
-        // alignItems: 'center',
-        // justifyContent: 'center',
-        // flexDirection: 'column'
-        alignItems: 'center',
+  },
+  skillsCon: {
+    width: 250,
+    height: 20,
+    margin: "0 20px 20px 20px",
+  },
+  skillsBar: {
+    transition: "0.5s",
+    height: 20,
+    background:
+      "linear-gradient(90deg, rgba(83,100,195,1) 9%, rgba(117,178,239,1) 92%)",
+  },
+  profileBg: {
+    background:
+      "linear-gradient(0deg, rgba(83,100,195,1) 8%, rgba(117,178,237,1) 90%)",
+  },
+  loadingContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "200px",
+  },
+  contentlistcon: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    justifyContent: "space-around",
+  },
+  postlistcon: {
+    height: "auto",
+    overflowY: "none",
+    [theme.breakpoints.up("sm")]: {
+      height: "60vh",
+      overflowY: "scroll",
     },
-    listItem: {
-        [theme.breakpoints.up('md')]: {
-            margin: 10,
-        },
-        display: 'flex',
-        margin: 5,
-        padding: 10,
-        transition: '0.3s',
-        '&:hover': {
-            margin: 2,
-            boxShadow: '0 5px 10px grey'
-        }
+  },
+  btn: {
+    "&:hover": {
+      cursor: "pointer",
     },
-    skillsCon: {
-        width: 250,
-        height: 20,
-        margin: '0 20px 20px 20px'
+  },
+  container: {
+    padding: "5px 15px 5px 15px",
+    [theme.breakpoints.up("sm")]: {
+      padding: "10px 25px 10px 25px",
     },
-    skillsBar: {
-        transition: '0.5s',
-        height: 20,
-        background: 'linear-gradient(90deg, rgba(83,100,195,1) 9%, rgba(117,178,239,1) 92%)'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
     },
-    profileBg: {
-        background: 'linear-gradient(0deg, rgba(83,100,195,1) 8%, rgba(117,178,237,1) 90%)'
+  },
+  tab: {
+    display: "block",
+    padding: "12px",
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.black, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.black, 0.25),
     },
-    loadingContainer: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '200px'
+    marginLeft: 0,
+    paddingLeft: 15,
+    width: "100% !important",
+    [theme.breakpoints.up("sm")]: {
+      width: "auto",
     },
-    contentlistcon: {
-        display: 'flex',
-        flexDirection: 'column',
-        height: '100%',
-        justifyContent: 'space-around'
-    
+  },
+  iconCircle: {
+    backgroundColor: "red",
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 200,
     },
-    postlistcon: {
-        height: 'auto',
-        overflowY: 'none',
-        [theme.breakpoints.up('sm')]: {
-            height: '80vh',
-            overflowY: 'scroll',
-        },
+  },
+  loginContainer: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 10,
+    width: "200px",
+  },
+  inputLogin: {
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.black, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.black, 0.25),
     },
-    btn: {
-        '&:hover': {
-            cursor: 'pointer',
-        }
+    marginRight: theme.spacing(2),
+    padding: 5,
+    width: "100%",
+  },
+  loginBtn: {
+    marginTop: 5,
+    backgroundColor: theme.palette.primary.main,
+    "&:hover": {
+      backgroundColor: theme.palette.primary.main,
     },
-    container: {
-        padding: '5px 15px 5px 15px',
-        [theme.breakpoints.up('sm')]: {
-            padding: '10px 25px 10px 25px'
-        },
+    color: "white",
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
     },
-    title: {
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
-    },
-    tab: {
-        display: 'block',
-        padding: '12px'
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.black, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.black, 0.25),
-        },
-        marginLeft: 0,
-        paddingLeft: 15,
-        width: '100% !important',
-        [theme.breakpoints.up('sm')]: {
-            width: 'auto',
-        },
-    },
-    iconCircle: {
-        backgroundColor: 'red'
-    },
-    searchIcon: {
-        width: theme.spacing(7),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 7),
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: 200,
-        },
-    },
-    loginContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 10,
-        width: '200px'
-    },
-    inputLogin: {
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.black, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.black, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        padding: 5,
-        width: '100%',
-    },
-    loginBtn: {
-        marginTop: 5,
-        backgroundColor: theme.palette.primary.main,
-        '&:hover': {
-            backgroundColor: theme.palette.primary.main,
-        },
-        color: 'white'
-    },
-    sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-            display: 'flex',
-        },
-    },
-    sectionMobile: {
-        display: 'flex',
-        [theme.breakpoints.up('md')]: {
-            display: 'none',
-        },
-    },
-}));
+  },
+}))
 
-export default useStyles;
+export default useStyles
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { Grid, Paper } from '@material-ui/core';
+import { Grid, Paper } from "@material-ui/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -13,12 +13,21 @@ class NotFoundPage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="404: Not Found" />
         <Grid container spacing={2} style={{ padding: 20 }}>
-        <Grid item xs={12} md={12}>
-          <Paper style={{minHeight:'80vh',display:'flex',alignContent:'center',alignItems:'center',justifyContent:'center',flexDirection:'column'}}>
-            <h1>Not Found</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-          </Paper>
-        </Grid>
+          <Grid item xs={12} md={12}>
+            <Paper
+              style={{
+                minHeight: "80vh",
+                display: "flex",
+                alignContent: "center",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+              }}
+            >
+              <h1>Not Found</h1>
+              <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            </Paper>
+          </Grid>
         </Grid>
       </Layout>
     )
