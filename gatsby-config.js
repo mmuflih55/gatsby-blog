d564490498acca83703aed7f4c654d04d434bd04
apem <m_muflih@hotmail.com>
2020-09-05 14:45:24 +0700

    fix md

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,7 +98,6 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // edit below
         icon: `content/assets/icon.png`,
       },
     },
